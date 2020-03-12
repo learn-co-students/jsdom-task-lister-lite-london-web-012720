e2fa80ca5efc7941d84fadca45ea9b54e0f11125
Sergio Robles <sroblesc@gmail.com>
2020-03-12 21:29:23 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+  const PRIORITY = {
+    1: "HIGH",
+    2: "MEDIUM",
+    3: "LOW"
+  };
+
+  const forma = document.getElementById("create-task-form")
+  const task = document.getElementById("new-task-description")
+  const newTask = document.getElementById("tasks")
+
+  forma.addEventListener("submit", event => {
+    event.preventDefault();
+
+    const newList = document.createElement("li")
+    newList.innerText = `${task.value}`
+
+    const deleteButton = document.createElement("button")
+    deleteButton.innerText = "DONE"
+
+    deleteButton.addEventListener("click", () => {
+      newList.remove();
+      deleteButton.remove()
+    })
+    newTask.append(newList, deleteButton)
+  })
+
+  const prioritySpan = document.createElement("span")
+  prioritySpan.innerText = PRIORITY[task.priority]
+
+})
