e7cfb2cadcf28b53ac7eec4e7f180cba57beecd2
Sergio Robles <sroblesc@gmail.com>
2020-05-01 16:27:15 +0100

    Saved

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
   const forma = document.getElementById("create-task-form")
-  const task = document.getElementById("new-task-description")
-  const newTask = document.getElementById("tasks")
+  const newTaskDescription = document.getElementById("new-task-description")
+  const task = document.getElementById("tasks")
 
   forma.addEventListener("submit", event => {
     event.preventDefault();
 
     const newList = document.createElement("li")
-    newList.innerText = `${task.value}`
+    newList.innerText = `${newTaskDescription.value}`
 
     const deleteButton = document.createElement("button")
     deleteButton.innerText = "DONE IT ALREADY"
@@ -17,13 +17,14 @@ document.addEventListener("DOMContentLoaded", () => {
     deleteButton.addEventListener("click", () => {
       newList.remove();
       deleteButton.remove()
-
-      
-
     })
-    newTask.append(newList, deleteButton)
+
+    task.append(newList, deleteButton)
     
   })
+
+
+
 
 
 })
